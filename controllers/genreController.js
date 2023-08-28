00bd181282969cfaf1b3701cd29f38e3d662b52f
Krichen Yassine <121350625+Yassinekrn@users.noreply.github.com>
2023-08-28 13:14:58 +0100

    finshed controllers + updated views

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -125,10 +125,55 @@ exports.genre_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Genre update GET");
+  const genre = await Genre.findById(req.params.id).exec();
+  if (genre === null) {
+    // No results.
+    const err = new Error("Genre not found");
+    err.status = 404;
+    return next(err);
+  }
+  res.render("genre_form", { title: "Update Genre", genre: genre });
+  
 });
 
+
 // Handle Genre update on POST.
-exports.genre_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Genre update POST");
-});
+exports.genre_update_post = [
+  // Validate and sanitize the name field.
+  body("name", "Genre name must contain at least 3 characters")
+      .trim()
+      .isLength({ min: 3 })
+      .escape(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+      const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+      const genre = new Genre({ name: req.body.name,
+        _id: req.params.id, });
+
+      if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("genre_form", {
+          title: "Create Genre",
+          genre: genre,
+          errors: errors.array(),
+      });
+      return;
+  } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      const genreExists = await Genre.findOne({ name: req.body.name }).exec();
+      if (genreExists) {
+        // Genre exists, redirect to its detail page.
+          res.redirect(genreExists.url);
+      } else {
+          await Genre.findByIdAndUpdate(req.params.id, genre, {});
+        // genre updated. Redirect to genre detail page.
+          res.redirect(genre.url);
+      }
+  }
+  }),
+];
--- models/author.js
+++ models/author.js
@@ -42,5 +42,15 @@ AuthorSchema.virtual("lifespan").get(function () {
   return `${this.date_of_birth_formatted} - ${this.date_of_death_formatted}`;
 });
 
+AuthorSchema.virtual("date_of_birth_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate(); // format 'YYYY-MM-DD'
+
+});
+
+AuthorSchema.virtual("date_of_death_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate(); // format 'YYYY-MM-DD'
+
+});
+
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
